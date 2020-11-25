69ff81cb4f852b3ff73c83ba07f8e8ab01d2fcf5
bkRodrigo <rodrigo@brewkrafts.com>
2020-11-25 13:39:39 -0300

    uncomment seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(AddressesSeeder::class);
+        $this->call(AddressesSeeder::class);
         $this->call(ContactsSeeder::class);
     }
 }
